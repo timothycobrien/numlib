75e7930f2102bcf3d4674790b501c2c36e60a1ec
luke-bhan <luke.bhan@vanderbilt.edu>
2020-01-13 15:26:30 -0600

    renamed files

--- examples/integrate_ex.rs
+++ examples/integrate_ex.rs
@@ -3,7 +3,7 @@ use numlib::integrate::*;
 fn main() {
     println!("Demonstration of the trapezoid quadrature and simpson quadrature of x^2 over [-1, 1]. The exact solution is 2/3.");
     println!("Trapezoid solution {}", composite_trapezoid(-1.0, 1.0, 100, square));
-    println!("Simpson's solution {}", simpson_quadrature(50.0, 1.0, 100, square)); 
+    println!("Simpson's solution {}", simpson_quadrature(-1.0, 1.0, 100, square)); 
 }
 
 fn square(x : f64) -> f64{
