6c08221da118030e0cd4b1b13e312fcd72d5417c
luke <luke.bhan@vanderbilt.edu>
2020-01-12 21:47:24 -0600

    added simpson quad

--- src/integrate.rs
+++ src/integrate.rs
@@ -9,7 +9,7 @@
  * b - the final endpoint
  * Returns:
  * The approximate solution to the integral
-*/
+ */
 
 pub fn composite_trapezoid(a: f64, b: f64, n: i64, f: fn(f64) -> f64) -> f64{
     let h : f64 = (b-a)/(n as f64);
@@ -20,4 +20,24 @@ pub fn composite_trapezoid(a: f64, b: f64, n: i64, f: fn(f64) -> f64) -> f64{
     return h*(sum + f(a)/(2 as f64) + f(b)/(2 as f64));
 }
 
+/* Simpsons Rule 
+ * Function: Computes an integral estimation using simpsons rule. Preferable
+ * to the trapezoidal rule in terms of error except on periodically smooth functions
+ * Parameters: 
+ * f - the function,
+ * n - the number of subintervals,
+ * a - the initial endpoint, 
+ * b - the final endpoint
+ * Returns:
+ * The approximate solution to the integral
+ *
+ */
 
+pub fn simpson_quadrature(a: f64, b: f64, n:i64, f: fn(f64) -> f64) -> f64{
+    let h : f64 = (b-a)/(n as f64);
+    let mut sum :f64 = 0.0;
+    for i in 1..(n-1){
+        sum += 2.0*f(a+(i as f64)*h) + 4.0*f(a+(i as f64)*h/(2 as f64));
+    }
+    return h*(sum + f(a) + f(b))/(6.0);
+}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -9,3 +9,14 @@ fn trapezoid_quad() {
     let a : f64 =  0.66679;
     assert_approx_eq!(a, composite_trapezoid(-1.0, 1.0, 100, square), 2 as f64);
 }
+
+#[test]
+fn simpson_test() {
+    fn square(x: f64) -> f64 {
+        return x*x
+    }
+    let a : f64 = 0.66679;
+    assert_approx_eq!(a, simpson_quadrature(-1.0, 1.0, 100, square), 2 as f64);
+}
+
+
